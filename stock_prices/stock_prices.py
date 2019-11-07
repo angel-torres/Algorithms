6bfd70c1f1a6ac460aa4831d2e176a7448ab40d5
angel-torres <alas4224@gmail.com>
2019-11-06 23:47:39 -0600

    gc

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,38 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+	curr_min = prices[0]
+	curr_max = prices[1]
+	curr_max_index = 0
+
+	for index, price in enumerate(prices):
+		if price > curr_max and index > 0:
+			curr_max = price	
+			curr_max_index = index
+
+	print(curr_min, curr_max)
+	for index, price in enumerate(prices):
+		if price < curr_min and index < curr_max_index:
+			curr_min = price
+	
+	return curr_max - curr_min
+
+
+
+
+prices_list = [1050, 270, 1540, 3800, 2]
+
+print(find_max_profit(prices_list))
+
+
+
+
 
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+	parser = argparse.ArgumentParser(description='Find max profit from prices.')
+	parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+	args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+	print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
