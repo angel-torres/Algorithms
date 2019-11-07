8fc9b71872c11be4c1224c30c9ac9ad801d87f5c
angel-torres <alas4224@gmail.com>
2019-11-07 17:48:37 -0600

    finished stock_prices and recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,40 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+	# loop over all recipe ingredients and check how many times they divide into
+	# the ingredients available
+	batches = 0	
+	batch_dict = {}
 
+	for ingredient in recipe:
+		try: ingredients[ingredient]
+		except KeyError: ingredients[ingredient] = None
+		if ingredients[ingredient] == None:
+			return 0
+		
+		if ingredients[ingredient] // recipe[ingredient] < 1:
+			return 0	 
+		batch_dict[ingredient] = ingredients[ingredient] // recipe[ingredient]
+		
+	values = list(batch_dict.values())
+	lowest_batch_num = values[0]
+	for batch_num in batch_dict:
+		if batch_dict[batch_num] < lowest_batch_num:
+			lowest_batch_num = batch_num
+
+	return lowest_batch_num
+
+	
+	
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+	# Change the entries of these dictionaries to test 
+	# your implementation with different inputs
+	recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+	ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+	print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 132, 'butter': 50, 'flour': 51 }
+
+print(recipe_batches(recipe, ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,33 +3,20 @@
 import argparse
 
 def find_max_profit(prices):
-	curr_min = prices[0]
-	curr_max = prices[1]
-	curr_max_index = 0
+	curr_min = prices[0] # initialize curr min to first value in list
+	curr_max = prices[1] # initialize curr max to second value in list 
+	curr_max_index = 0 # initialize the index of the current max
 
-	for index, price in enumerate(prices):
-		if price > curr_max and index > 0:
-			curr_max = price	
+	for index, price in enumerate(prices): # loop over list to find max value and it's index
+		if price > curr_max and index > 0: # update max value and max index if price is
+			curr_max = price	           # greater than curr max value and index is > 0
 			curr_max_index = index
 
-	print(curr_min, curr_max)
-	for index, price in enumerate(prices):
-		if price < curr_min and index < curr_max_index:
+	for index, price in enumerate(prices): # loop over to find min value that comes before
+		if price < curr_min and index < curr_max_index: # the curr_max_index
 			curr_min = price
 	
 	return curr_max - curr_min
-
-
-
-
-prices_list = [1050, 270, 1540, 3800, 2]
-
-print(find_max_profit(prices_list))
-
-
-
-
-
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
